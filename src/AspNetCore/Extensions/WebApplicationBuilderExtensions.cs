c97cc3280058874f3c78cec832a994226838511c
EndarValuk <endarvaluk@gmail.com>
2024-05-16 18:08:36 +0500

    ASpNetCore app setup shorthand extensions added

---
+++ src/AspNetCore/Extensions/WebApplicationBuilderExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Auriga.Toolkit.Plugins;
+
+namespace Auriga.Toolkit.AspNetCore.Extensions;
+
+/// <summary>
+/// Web application builder helper methods extensions.
+/// </summary>
+public static class WebApplicationBuilderExtensions
+{
+	/// <summary>
+	/// Executes plugins-based workflow and builds web application.
+	/// </summary>
+	/// <param name="builder">Web application builder.</param>
+	/// <returns><see cref="WebApplication"/> so that additional calls can be chained.</returns>
+	public static WebApplication BuildApplication(this WebApplicationBuilder builder)
+	{
+		ArgumentNullException.ThrowIfNull(builder);
+
+		_ = builder.Host.ExecutePluginsPrepareOperations(builder.Configuration, builder.Logging);
+
+		_ = builder.Configuration.ExecutePluginsCheckConfiguration();
+
+		_ = builder.Services.ExecutePluginsConfigureServices(builder.Configuration);
+
+		WebApplication app = builder.Build();
+
+		_ = app.ExecutePluginsConfigurePipeline();
+
+		_	= app.ExecutePluginsConfigureRouting();
+
+		_ = app.ExecutePluginsPostOperations();
+
+		return app;
+	}
+}
